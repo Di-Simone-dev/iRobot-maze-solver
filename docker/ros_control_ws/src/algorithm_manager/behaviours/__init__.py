e488bb8afedc13d8543e28091a6290c3abff3820
Ghigolini <jacsalv@outlook.it>
2025-12-02 18:45:38 +0100

    Aggiunta messaggio per algoritmo

---
+++ docker/ros_control_ws/src/algorithm_manager/behaviours/__init__.py
@@ -0,0 +1,6 @@
+from .AlgorithmSelector import AlgorithmSelector
+from .MoveForward import MoveForward
+from .ChooseDirection import ChooseDirection
+from .CheckExit import CheckExit
+from .pledge import *
+from .tremaux import *
